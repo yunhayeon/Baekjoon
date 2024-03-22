0bff01f2883215c5247068b7b83294f268b7a8b4
yunhayeon <121076117+yunhayeon@users.noreply.github.com>
2024-03-22 17:24:07 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 112076 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def perm(level, used):
+    if level == M:
+        print(*used)
+        return
+    
+    for i in range(N):
+        if not used:
+            perm(level + 1, used + [num[i]])
+        elif used[-1] <= num[i]:
+            perm(level + 1, used + [num[i]])
+
+N, M = map(int, input().split())
+num = [i for i in range(1, N + 1)]
+
+perm(0, [])
