f8cfdcf10882846593247ce57581eafb700b20e0
yunhayeon <121076117+yunhayeon@users.noreply.github.com>
2024-03-22 17:59:29 +0900

    [Silver III] Title: N과 M (7), Time: 2092 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,15 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def perm(level, used):
+def perm(level):
     if level == M:
         print(*used)
         return
     
     for i in range(N):
-        perm(level + 1, used + [lst[i]])
+        used[level] = lst[i]
+        perm(level + 1)
 
 N, M = map(int, input().split())
 lst = sorted(list(map(int, input().split())))
+used = [0] * M
 
-perm(0, [])
+perm(0)
