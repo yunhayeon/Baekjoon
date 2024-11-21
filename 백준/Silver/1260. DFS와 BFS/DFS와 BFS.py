1bda792034356148f0159251ae1e1c826ac2e7b6
yunhayeon <121076117+yunhayeon@users.noreply.github.com>
2024-11-21 11:43:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,59 @@
+import sys
+input = sys.stdin.readline
+
+N, M, V = map(int, input().split())
+lst = [[] for _ in  range(N + 1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    lst[i].append(j)
+    lst[j].append(i)
+
+for i in range(N + 1):
+    lst[i].sort()
+
+
+def dfs(s):
+    visited = [0 for _ in range(N + 1)]
+    visited[s] = 1
+    stack = []
+    ans = [s]
+    now = s
+
+    while True:
+        for next in lst[now]:
+            if not visited[next]:
+                stack.append(now)
+                visited[next] = 1
+                ans.append(next)
+                now = next
+                break
+        else:
+            if stack:
+                now = stack.pop()
+            else:
+                break
+
+    return ans
+
+
+def bfs(s):
+    visited = [0 for _ in range(N + 1)]
+    visited[s] = 1
+    q = [s]
+    ans = [s]
+    
+    while q:
+        now = q.pop(0)
+
+        for next in lst[now]:
+            if not visited[next]:
+                q.append(next)
+                ans.append(next)
+                visited[next] = 1
+    
+    return ans
+
+
+print(*dfs(V))
+print(*bfs(V))
